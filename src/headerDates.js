b816a5c89916a5177ce708c5f891a38cf3c6d50c
pour-moi <pour64moi@gmail.com>
2023-11-11 22:14:19 +0300

    list of times added

--- src/headerDates.js
+++ src/headerDates.js
@@ -1,10 +1,22 @@
-import { format, addDays } from "date-fns";
+import { format, addDays, addHours } from "date-fns";
+
+const generateTimeList = (startTime, endTime, interval) => {
+  const timeList = [];
+  let currentTime = startTime;
+
+  while (currentTime <= endTime) {
+    timeList.push(format(currentTime, "h a"));
+    currentTime = addHours(currentTime, interval);
+  }
+
+  return timeList;
+};
 
 export const headerDates = () => {
   const listDate = document.querySelector(".days");
   const CurrentDate = document.querySelector(".theDate");
   let currentDate = new Date();
-  CurrentDate.textContent = format(currentDate, "MMMM do, yyyy");
+  CurrentDate.textContent = format(currentDate, "MMM d, yyyy");
   let dateNumber = format(currentDate, "dd");
 
   for (let i = 0; i < 7; i++) {
@@ -28,4 +40,24 @@ export const headerDates = () => {
 
     listDate.appendChild(dateElement);
   }
+
+  // Example: Generate a list of times from the current time to 12:00 PM with a 1-hour interval
+  const startTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0); // Current day, 12:00 AM
+  const endTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 12, 0); // Current day, 12:00 PM
+  const interval = 1; // 1-hour interval
+
+  const timeList = generateTimeList(startTime, endTime, interval);
+
+  // Assuming you have an element to display the time list, update it accordingly
+  const timeListContainer = document.querySelector(".time-list");
+
+  // Clear existing content in case the function is called multiple times
+  timeListContainer.innerHTML = "";
+
+  timeList.forEach((time) => {
+    const timeItem = document.createElement("div");
+    timeItem.classList.add("time-item");
+    timeItem.textContent = time;
+    timeListContainer.appendChild(timeItem);
+  });
 };
