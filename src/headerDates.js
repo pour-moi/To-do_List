3b4b7c60817fb69afb02e94a0bf9f21ff74961b4
pour-moi <pour64moi@gmail.com>
2023-11-11 23:16:03 +0300

    time dash added

--- src/headerDates.js
+++ src/headerDates.js
@@ -42,8 +42,20 @@ export const headerDates = () => {
   }
 
   // Example: Generate a list of times from the current time to 12:00 PM with a 1-hour interval
-  const startTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0); // Current day, 12:00 AM
-  const endTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 12, 0); // Current day, 12:00 PM
+  const startTime = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate(),
+    0,
+    0
+  ); // Current day, 12:00 AM
+  const endTime = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate(),
+    12,
+    0
+  ); // Current day, 12:00 PM
   const interval = 1; // 1-hour interval
 
   const timeList = generateTimeList(startTime, endTime, interval);
@@ -55,9 +67,19 @@ export const headerDates = () => {
   timeListContainer.innerHTML = "";
 
   timeList.forEach((time) => {
+    // Create a new div as a container
+    const timeContainer = document.createElement("div");
+    timeContainer.classList.add("time-container");
+
     const timeItem = document.createElement("div");
     timeItem.classList.add("time-item");
     timeItem.textContent = time;
-    timeListContainer.appendChild(timeItem);
+    timeContainer.appendChild(timeItem); // Append to the container
+
+    const dashedLine = document.createElement("div");
+    dashedLine.classList.add("dashed-line");
+    timeContainer.appendChild(dashedLine); // Append to the container
+
+    timeListContainer.appendChild(timeContainer); // Append the container
   });
 };
