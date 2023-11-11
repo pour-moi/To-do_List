83951896877a21c47ecf75b636fde5cd33d6a755
pour-moi <pour64moi@gmail.com>
2023-11-11 22:14:36 +0300

    adding task button added

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import "./style.css";
 import { headerDates } from "./headerDates.js";
 
 headerDates();
+
+const addButton = document.querySelector(".plusSign");
+const test = document.querySelector(".test");
+const newTask = document.querySelector(".new_Task");
+
+addButton.addEventListener("click", function(){
+    test.style.display = "none";
+    newTask.style.display = "block";
+})
